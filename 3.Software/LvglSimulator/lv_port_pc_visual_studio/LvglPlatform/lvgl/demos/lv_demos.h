261facecb8e6fffb32ec83f92d87745bb4efc4bc
nekocharm <zhangchi.mail@qq.com>
2024-10-12 23:23:28 +0800

    update lvgl simulator to lvgl 9.3

---
+++ 3.Software/LvglSimulator/lv_port_pc_visual_studio/LvglPlatform/lvgl/demos/lv_demos.h
@@ -0,0 +1,55 @@
+﻿/**
+ * @file lv_demos.h
+ *
+ */
+
+#ifndef LV_DEMOS_H
+#define LV_DEMOS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+#include "../lvgl.h"
+
+#if LV_USE_DEMO_WIDGETS
+#include "widgets/lv_demo_widgets.h"
+#endif
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+
+/**
+ * Call lv_demo_xxx.
+ * @param   info the information which contains demo name and parameters
+ *               needs by lv_demo_xxx.
+ * @size    size of information.
+ */
+bool lv_demos_create(char * info[], int size);
+
+/**
+ * Show help for lv_demos.
+ */
+void lv_demos_show_help(void);
+
+/**********************
+ *      MACROS
+ **********************/
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /*LV_DEMO_H*/
